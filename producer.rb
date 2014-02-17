d6505ff427be4181232cb44879b0838790123591
Andy Goldstein <andy.goldstein@gmail.com>
2014-02-17 18:16:20 -0500

    Switch from delay to approx rate

--- producer.rb
+++ producer.rb
@@ -1,11 +1,20 @@
 #!/bin/env ruby
 
 message_length = ARGV[0].to_i
-delay = ARGV[1].to_f
+message_rate = ARGV[1].to_f
 
 i = 0
+start = Time.now
+interval = 1.0 / message_rate if message_rate > 0
 loop do
   i += 1
   puts "#{i} #{'a' * message_length}"
-  sleep delay
+
+  if message_rate > 0
+    wait_till = start + (i * interval)
+    delay = wait_till - Time.now
+    if delay > 0
+      sleep(delay)
+    end
+  end
 end
